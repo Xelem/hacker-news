ef6e1609df070896b8346c03607054ad52e04aa4
Xelem <anselmchizurum@gmail.com>
2022-06-23 15:02:01 +0100

    Fetched the homepage data

--- comps/News.js
+++ comps/News.js
@@ -1,32 +1,40 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 
-const News = () => {
-  const baseUrl = "http://hn.algolia.com/api/v1/";
+const News = ({ children }) => {
+  const [baseUrl, setBaseUrl] = useState(
+    "https://hn.algolia.com/api/v1/search_by_date?tags=front_page"
+  );
 
   return (
     <>
       <div className="news">
-        <div className="news-card">
-          <Image
-            className="image"
-            src="/white.jpeg"
-            alt="Plain white image"
-            width="300"
-            height="200"
-          ></Image>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet
-            amet soluta ducimus, dolorum sint tempora?
-          </p>
-          <div className="info">
-            <Link href="/news/:id">
-              <button>Read more</button>
-            </Link>
-            <p>5 mins ago</p>
+        {children.map((child) => (
+          <div className="news-card" key={child.objectID}>
+            <Image
+              className="image"
+              src="/white.jpeg"
+              alt="Plain white image"
+              width="300"
+              height="200"
+            ></Image>
+            <p>
+              {child.title}- <i>{child.author}</i>
+            </p>
+            <div className="info">
+              <Link href="/news/:id">
+                <button>Read more</button>
+              </Link>
+              <p>
+                {child.created_at.split("T")[0]} ||{" "}
+                {child.created_at.split("T")[1].slice(0, -5)}
+              </p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,36 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 import Head from "next/head";
 import News from "../comps/News";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const res = await axios.get(
+    "https://hn.algolia.com/api/v1/search_by_date?tags=front_page"
+  );
+  const data = await res.data;
+
+  return {
+    props: { news: data.hits },
+  };
+};
+
+export default function Home({ news }) {
+  const [baseUrl, setBaseUrl] = useState(
+    "https://hn.algolia.com/api/v1/search_by_date?tags=front_page"
+  );
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const res = await axios.get(baseUrl);
+  //     const data = await res.data;
+
+  //     console.log(data.hits);
+  //   };
+  //   getData();
+  // }, [baseUrl]);
+
   return (
     <div>
       <div className="search-bar">
@@ -14,7 +42,7 @@ export default function Home() {
         </select>
       </div>
       <div className="content">
-        <News />
+        <News>{news}</News>
       </div>
     </div>
   );
