8167e0be5ed1f2e8ee3ba3ece0cdc107c498a8a0
Xelem <anselmchizurum@gmail.com>
2022-06-23 21:54:46 +0100

    Added the title

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <title>Hacker News | Search</title>
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
