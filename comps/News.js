11d2ff8e4b814623cb20522614dc4bbc6417e488
Xelem <anselmchizurum@gmail.com>
2022-07-04 23:19:11 +0100

    Completed the paginate feature

--- comps/News.js
+++ comps/News.js
@@ -1,21 +1,32 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import ReactPaginate from "react-paginate";
 import NewsCard from "./NewsCard";
 
 const News = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [articles, setArticles] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handlePageChange = (e) => {
+    setCurrentPage(e.selected);
+    console.log(e);
+  };
 
   useEffect(() => {
     setIsLoading(true);
     const getData = async () => {
-      const { data } = await axios.get("http://hn.algolia.com/api/v1/search?");
+      const { data } = await axios.get("http://hn.algolia.com/api/v1/search?", {
+        params: { page: currentPage },
+      });
       const { hits, nbPages } = data;
       setArticles(hits);
       setIsLoading(false);
+      setPageCount(nbPages);
     };
     getData();
-  }, []);
+  }, [currentPage]);
 
   return (
     <>
@@ -36,8 +47,20 @@ const News = () => {
               <NewsCard key={article.objectID} article={article} />
             ))}
         </div>
+        <ReactPaginate
+          nextLabel=">>"
+          previousLabel="<<"
+          breakLabel="..."
+          forcePage={currentPage}
+          pageCount={pageCount}
+          renderOnZeroPageCount={null}
+          onPageChange={handlePageChange}
+          className="paginate"
+          activeClassName="active-page"
+          previousClassName="previous-page"
+          nextClassName="next-page"
+        />
       </div>
-      <div className="content"></div>
     </>
   );
 };
