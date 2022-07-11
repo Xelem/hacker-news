79706e1713ab080445a706de12cb0a9948ef54c7
Xelem <anselmchizurum@gmail.com>
2022-07-11 23:35:37 +0100

    Reverting to e884345 again

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,13 @@
-import axios from "axios";
 import Head from "next/head";
 import News from "../comps/News";
 
-export const getStaticProps = async () => {
-  const { data } = await axios.get("http://hn.algolia.com/api/v1/search?");
-  const { hits, nbPages } = data;
-
-  return {
-    props: { hits, nbPages },
-  };
-};
-
-export default function Home({ hits, nbPages }) {
+export default function Home() {
   return (
     <div>
       <Head>
         <title>Hacker News | Search</title>
       </Head>
-      <News hits={hits} nbPages={nbPages} />
+      <News />
     </div>
   );
 }
