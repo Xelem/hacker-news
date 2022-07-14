17714096ba0665c2c9a2e1a48fc71983c0009933
Xelem <anselmchizurum@gmail.com>
2022-07-14 18:54:34 +0100

    Used another way of filtering that is Chrome compatible

--- comps/CommentCard.js
+++ comps/CommentCard.js
@@ -1,6 +1,26 @@
 const CommentCard = (comment) => {
   if (!comment.comment.text) return null;
-  const commentText = comment.comment.text;
+  const commentText = comment.comment.text
+    .replace(/<p>/g, " ")
+    .replace("</p>", " ")
+    .replace(/<a>/g, " ")
+    .replace("</a>", " ")
+    .replace(/&#x2F;/g, " ")
+    .replace(/<a href=/g, " ")
+    .replace(/rel=/g, " ")
+    .replace(/>/g, " ")
+    .replace(/nofollow/g, " ")
+    .replace(/"/g, "")
+    .replace(/&quot;/g, " ")
+    .replace(/=/g, "")
+    .replace(/https:/g, "")
+    .replace(/www./g, "www. ")
+    .replace(/.com/g, " .com ")
+    .replace(/_/g, " _ ")
+    .replace(/&amp;/g, " ")
+    .replace(/./g, " . ")
+    .replace(/%/g, " ")
+    .replace("/", " / ");
 
   return (
     <div className="comment-card">
@@ -11,7 +31,7 @@ const CommentCard = (comment) => {
         </p>
         <p>
           <i>
-            Created At: {comment.comment.created_at.split("T")[0]} ||{" "}
+            Created At: {comment.comment.created_at.split("T")[0]} || ,
             {comment.comment.created_at.split("T")[1].slice(0, -5)}
           </i>
         </p>
