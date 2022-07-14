fef185f9b4f6662918b8e293d6c025ef309d6611
Xelem <anselmchizurum@gmail.com>
2022-07-14 18:42:05 +0100

    Changed replaceAll to replace

--- comps/CommentCard.js
+++ comps/CommentCard.js
@@ -1,25 +1,26 @@
 const CommentCard = (comment) => {
   if (!comment.comment.text) return null;
   const commentText = comment.comment.text
-    .replaceAll("<p>", " ")
-    .replaceAll("</p>", " ")
-    .replaceAll("<a>", " ")
-    .replaceAll("</a>", " ")
-    .replaceAll("&#x2F;", " ")
-    .replaceAll("<a href=", " ")
-    .replaceAll("rel=", " ")
-    .replaceAll(">", " ")
-    .replaceAll("nofollow", " ")
-    .replaceAll('"', "")
-    .replaceAll("&quot;", " ")
-    .replaceAll("=", "")
-    .replaceAll("https:", "")
-    .replaceAll("www.", "www. ")
-    .replaceAll(".com", " .com ")
-    .replaceAll("_", " _ ")
-    .replaceAll("&amp;", " ")
-    .replaceAll(".", " . ")
-    .replaceAll("%", " ");
+    .replace("<p>", " ")
+    .replace("</p>", " ")
+    .replace("<a>", " ")
+    .replace("</a>", " ")
+    .replace("&#x2F;", " ")
+    .replace("<a href=", " ")
+    .replace("rel=", " ")
+    .replace(">", " ")
+    .replace("nofollow", " ")
+    .replace('"', "")
+    .replace("&quot;", " ")
+    .replace("=", "")
+    .replace("https:", "")
+    .replace("www.", "www. ")
+    .replace(".com", " .com ")
+    .replace("_", " _ ")
+    .replace("&amp;", " ")
+    .replace(".", " . ")
+    .replace("%", " ")
+    .replace("/", " / ");
 
   return (
     <div className="comment-card">
