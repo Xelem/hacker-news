39bedc469848a087d803d4b3cfb13f4d54dfce16
Xelem <anselmchizurum@gmail.com>
2022-07-14 18:44:07 +0100

    Removed all the filters

--- comps/CommentCard.js
+++ comps/CommentCard.js
@@ -1,26 +1,6 @@
 const CommentCard = (comment) => {
   if (!comment.comment.text) return null;
-  const commentText = comment.comment.text
-    .replace("<p>", " ")
-    .replace("</p>", " ")
-    .replace("<a>", " ")
-    .replace("</a>", " ")
-    .replace("&#x2F;", " ")
-    .replace("<a href=", " ")
-    .replace("rel=", " ")
-    .replace(">", " ")
-    .replace("nofollow", " ")
-    .replace('"', "")
-    .replace("&quot;", " ")
-    .replace("=", "")
-    .replace("https:", "")
-    .replace("www.", "www. ")
-    .replace(".com", " .com ")
-    .replace("_", " _ ")
-    .replace("&amp;", " ")
-    .replace(".", " . ")
-    .replace("%", " ")
-    .replace("/", " / ");
+  const commentText = comment.comment.text;
 
   return (
     <div className="comment-card">
